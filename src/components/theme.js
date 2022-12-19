0a78589cc986e7257c40f1e088148b4c00c346da
EkaterinaKononenko <kaktysik4@ukr.net>
2022-12-19 07:37:03 +0200

    add start task

---
+++ src/components/theme.js
@@ -0,0 +1,33 @@
+export const theme = {
+  colors: {
+    text: '#734222',
+    background: 'linear-gradient(45deg, #79553D, #755C48,  #BC987E)',
+    primary: '#4D220E',
+    secondary: '#79443B',
+    border: '#45322E',
+    light: '#D2B48C',
+  },
+  fonts: {
+    body: 'Raleway, sans-serif',
+  },
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 900,
+  },
+
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256, 512],
+
+  borders: {
+    heading: '4px solid #45322E',
+    light: '2px solid #D2B48C',
+  },
+  radii: {
+    heading: '10px',
+  },
+  shadows: {
+    boxShadow: ' box-shadow: -3px -2px 24px 8px rgba(184, 141, 12, 0.6) inset;',
+  },
+};
